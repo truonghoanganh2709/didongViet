6202734cc14f0fb58e8db052626e4459da273956
HOANG ANH <you@example.comtha27092005@gmail.com>
2026-05-20 18:01:53 +0700

    update auth and redis

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,6 +1,11 @@
+import { Redis } from "@upstash/redis";
 import dotenv from "dotenv";
-import Redis from "ioredis";
 
 dotenv.config();
 
-export const redis = new Redis(process.env.REDIS_URL);
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+console.log("✅ Redis initialized");
